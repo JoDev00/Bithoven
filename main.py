a4c8cc771ffe9a98a7c4da0c7e7367bf27f5a29d
JoDev00 <JoDev00@users.noreply.github.com>
2025-09-09 13:28:26 +0100

    Fixed issue with y start using incorrect value, added middle calculations

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import pyautogui
 import time
 
+TILE_SEGMENTS = 4
+
 def main():
     initialize_grid()
     #pyautogui.moveTo(center)
 
 def initialize_grid():
+    grid = []
+
     print("Place your cursor at the bottom left corner")
-    time.sleep(3)
-    x1 = pyautogui.position().x
-    y1 = pyautogui.position().x
-    print(f"x1 = {x1}, y1 = {y1}")
+    time.sleep(2)
+    x_start = pyautogui.position().x
+    y_start = pyautogui.position().y
 
     print("\nPlace your cursor at the top right corner")
-    time.sleep(3)
-    x2 = pyautogui.position().x
-    y2 = pyautogui.position().x
-    print(f"x2 = {x2}, y2 = {y2}")
-
     time.sleep(2)
+    x_end = pyautogui.position().x
+    y_end = pyautogui.position().y
 
-    # bask in the glory of my fantastic segmenting algorithm
-    for i in range(4):
-        time.sleep(0.5)
-        pyautogui.moveTo(x1 + ((x1 / 4) * i), 500)
+    print(f"x_start = {x_start}, y_start = {y_start}")
+    print(f"x_end = {x_end}, y_end = {y_end}")
+
+    middle = ((x_start + x_end) / 2, (y_start + y_end) / 2)
+    pyautogui.moveTo(middle)
+    print(f"moved to {middle}")
 
 if __name__ == '__main__':
     main()

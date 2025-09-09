7c343a629ffe3984d87f876ab88922892b505a6b
JoDev00 <JoDev00@users.noreply.github.com>
2025-09-09 13:52:26 +0100

    The code now correctly selects x coordinate centers in a grid

--- main.py
+++ main.py
@@ -23,9 +23,17 @@ def initialize_grid():
     print(f"x_start = {x_start}, y_start = {y_start}")
     print(f"x_end = {x_end}, y_end = {y_end}")
 
-    middle = ((x_start + x_end) / 2, (y_start + y_end) / 2)
-    pyautogui.moveTo(middle)
-    print(f"moved to {middle}")
+    grid_dimensions = (x_end - x_start, y_start - y_end)
+    # Then I need to half this to get the center of each tile, but do that *after* multiplying by 1-4
+    tile_width = (grid_dimensions[0] / TILE_SEGMENTS, grid_dimensions[1] / TILE_SEGMENTS)
+
+    print(f"grid_dimensions: {grid_dimensions}, tile_width: {tile_width}")
+    for i in range(4):
+        x_coordinate = (x_start + tile_width[0] * (i + 0.5))
+
+        print(x_coordinate)
+        pyautogui.moveTo(x_coordinate, pyautogui.position().y)
+        time.sleep(1)
 
 if __name__ == '__main__':
     main()

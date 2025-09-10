1d8e5e8d891a762a092c3b74b0597bbeab44d170
JoDev00 <JoDev00@users.noreply.github.com>
2025-09-10 17:03:10 +0100

    Grid matrix altered, better results already

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 import pyautogui
 import time
-import pyscreeze
 import sys
 import keyboard
 import mouse
+from PIL import ImageGrab
 
 def main():
-    window_dimensions = (200, 951), (640, 165)
-    grid_matrix = initialize_grid_matrix(window_dimensions)
+    grid_matrix = initialize_grid_matrix()
     scan_for_black_tiles(grid_matrix)
     game_loop(grid_matrix)
 
 def game_loop(grid_matrix):
     while not keyboard.is_pressed('q'):
         scan_for_black_tiles(grid_matrix)
-
     sys.exit()
 
 def scan_for_black_tiles(grid_matrix):
     # bottom to top (priority)
+    screenshot = ImageGrab.grab()
     for row in reversed(grid_matrix):
-        for coordinate in row:
-            if pyautogui.pixelMatchesColor(x=coordinate[0], y=coordinate[1], expectedRGBColor=(0, 0, 0)):
-                print(f"detected black tile coordinate: {coordinate}")
-                pyautogui.mouseDown(x=coordinate[0], y=coordinate[1], button="left")
-                time.sleep(0.01)
-                pyautogui.mouseUp(x=coordinate[0], y=coordinate[1], button="left")
+        for x, y in row:
+            pixel = screenshot.getpixel((x, y))
+            if pixel == (0, 0, 0):
+                click_coordinate(x, y)
+
+def click_coordinate(x, y, hold_duration = 0.01):
+    print(f"detected black tile coordinate: {(x, y)}")
+    pyautogui.mouseDown(x=x, y=y + 40, button="left")
+    pyautogui.mouseUp(button="left")
 
 # for now, i'll manually put in the tile positions, but it will be configurable later
-def initialize_grid_matrix(window_dimensions):
+def initialize_grid_matrix():
     # these get initialized to tuples later, but for readability this works for my purposes
 
     # this represents the tile coordinates to check
     grid_matrix = [
-        [(250, 200), (360, 200), (480, 200), (590, 200)],
-        [(250, 400), (360, 400), (480, 400), (590, 400)],
         [(250, 600), (360, 600), (480, 600), (590, 600)],
         [(250, 800), (360, 800), (480, 800), (590, 800)]
     ]
